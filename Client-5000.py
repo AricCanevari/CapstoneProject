4960d42525d12eb5d492bdfbedcc9b4c0eeb5be7
Aric Canevari <canevaa@sunyit.edu>
2016-03-01 17:54:31 -0500

    Update Client-5000.py

--- Client-5000.py
+++ Client-5000.py
@@ -23,4 +23,34 @@ print Incomming
 s.send(Address)
 s.send(UserName)
 s.send(ConnectTo)
-s.close()
+Incomming = s.recv(1024)
+if (Incomming == "true"):
+  Port = s.recv(1024)
+  s.close()
+  ServerS = socket.socket()
+  
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  s.connect(("gmail.com",80))
+  Address = (s.getsockname()[0])
+  s.close()
+
+  ServerS.bind((Address, Port))
+  a = 0 
+  ServerS.listen(5)
+  while (a < 2):
+  	Client, ClientAddr = ServerS.accept()
+  	print "Got Connection from", ClientAddr
+  	x = "Connected to: " + Address
+  	Client.send(x)
+  	ServerS.close()
+if (Incomming == "false):
+  ServerAddr = s.recv(1024)
+  Port = s.recv
+  s.close()
+  s = socket.socket()
+ 
+  s.connect((ServerAddr, Port))
+  Incomming = s.recv(1024)
+  print Incomming
+  s.close()
+print "All sockets Closed"
