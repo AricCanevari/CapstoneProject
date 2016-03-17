9622a3f1a9115cdfc9b7041843196d2a591f5288
Aric Canevari <canevaa@sunyit.edu>
2016-03-16 22:15:41 -0400

    Update Server-5000.py

--- Server-5000.py
+++ Server-5000.py
@@ -10,51 +10,52 @@ def get_local_ip():
 	s.close()
 	return Address
 	
-#Creates the Server Socket for messaging
-def create_connection():
+#Creates the Server Socket for messaging returns server socket
+def create_server_connection():
 	ServerS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	Port = 5000
 	ServerS.bind((Address, Port))
 	ServerS.listen(5)
 	return ServerS
 
+def connect_with_clients():
+	clientport = "5005"
+	a = 0
+	while (a < 2):
+		Client, ClientAddr = ServerS.accept()
+		print "Got Connection from", ClientAddr
+		x = "Connected to Server"
+		Client.send(x)
+		ClientIP = Client.recv(1024)
+		print "ClientIP recv"
+		ClientUN = Client.recv(1024)
+		print "ClientUN recv"
+#		ClientCT = Client.recv(1024) #Code is freezing here? why?? removing to test
+#		print "ClientCT recv"
+		if (a == 0):
+			print "Entered if a = 0"
+			outgoing = "true"
+			Client.send(outgoing)
+			print Client.recv(1024)
+			Client.send(clientport)
+			ClientAIP = ClientIP
+		if (a == 1):
+			print "Entered if a = 1"
+			outgoing = "false"
+			Client.send(outgoing)
+			print Client.recv(1024)
+			Client.send(ClientAIP)
+			print Client.recv(1024)
+			Client.send(clientport)
+		Client.close()
+		a=a+1
+	ServerS.close()
+	
 #------------------------------------------------------------
 	
 Address = get_local_ip()
-ServerS = create_connection()
-
-
-clientport = "5005"
-a = 0
-while (a < 2):
-	Client, ClientAddr = ServerS.accept()
-	print "Got Connection from", ClientAddr
-	x = "Connected to Server"
-	Client.send(x)
-	ClientIP = Client.recv(1024)
-	print "ClientIP recv"
-	ClientUN = Client.recv(1024)
-	print "ClientUN recv"
-#	ClientCT = Client.recv(1024) #Code is freezing here? why?? removing to test
-#	print "ClientCT recv"
-	if (a == 0):
-		print "Entered if a = 0"
-		outgoing = "true"
-		Client.send(outgoing)
-		print Client.recv(1024)
-		Client.send(clientport)
-		ClientAIP = ClientIP
-	if (a == 1):
-		print "Entered if a = 1"
-		outgoing = "false"
-		Client.send(outgoing)
-		print Client.recv(1024)
-		Client.send(ClientAIP)
-		print Client.recv(1024)
-		Client.send(clientport)
-	Client.close()
-	a=a+1
-ServerS.close()
+ServerS = create_server_connection()
+connect_with_clients()
 
 #Creates a connection to get public address of itself
 #Address = subprocess.check_output("wget http://people.sunyit.edu/~greenli/ip.php -qO -", shell=True)
