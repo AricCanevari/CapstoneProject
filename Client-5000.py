0b94aa0e92bbfc1651fc9e9b8700e108f145fadb
Aric Canevari <canevaa@sunyit.edu>
2016-03-16 23:18:32 -0400

    Update Client-5000.py

--- Client-5000.py
+++ Client-5000.py
@@ -52,7 +52,7 @@ def connect_to_server():
 		x = "Connected to: " + Address
 		Client.send(x)
 		ServerS.close()
-		return(Client, Port)
+		return(ClientAddr, Port)
 	else:
 		outgoing = "Client Entered Client Portion of Code"
   		print outgoing
