bb56d8b962f9f63814a020c8aa1f9a8abe7bf227
Aric Canevari <canevaa@sunyit.edu>
2016-03-25 17:37:54 -0400

    Update Client-5000.py

--- Client-5000.py
+++ Client-5000.py
@@ -14,7 +14,7 @@ def get_user_input():
 	Prompt = '>'
 	print "DDNS Host Name"
 	UserName = raw_input(Prompt)
-	UserName = (str(UserName), 25)
+	#UserName = (str(UserName), 25)
 	print UserName
 	print "Connect to?"
 	ConnectTo = raw_input(Prompt)
