22e359cca30c6668b9109369ac4c8b3aa09ec66b
Aric Canevari <canevaa@sunyit.edu>
2016-04-12 20:51:52 -0400

    Update Client-5000.py

--- Client-5000.py
+++ Client-5000.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
+import os
 import socket
 import subprocess
-from threading import Thread
+import pickle
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+
+#Open file for logging
+
+logfile = ""
 
 #Gets the local IP Address of the computer
 def get_local_ip():
@@ -9,171 +17,70 @@ def get_local_ip():
  	s.connect(("gmail.com",80))
  	Address = (s.getsockname()[0])
  	Address = str(Address)
- 	print "Address in function get_local_ip()" + Address
  	s.close()
  	return Address
- 	
- 	
-#Gets user input for connections and returns it
-def get_user_input():
-	Prompt = '>'
-	print "DDNS Host Name"
-	UserName = raw_input(Prompt)
-	UserName = str(UserName)
-	print UserName
-	print "Connect to?"
-	ConnectTo = raw_input(Prompt)
-	return(UserName, ConnectTo)
+ 	#End get_local_address
 
-#Used from the threading statment. It will continuously wait for a message
-def recvthread(mssg):
-	quit = False
-	data = ""
-	print mssg
-	if (mssg == 1):
-		global Client
-		while quit == False:
-			if (data == "quit"):
-				quit = True #not working, Why??
-			data = Client.recv(1024)
-			print "\r[Other]:" + data
-			
-	if (mssg == 2):
-		global cs
-		while True:
-			if (data == "quit"):
-				quit = True
-			data = cs.recv(1024)
-			print "\r[Other]:" + data
-			
+#check for hermes directory, create it if it doesnt exist
+def check_log_dir():
+	global logfile
+	if not os.path.exists('/var/log/hermes'):
+		print '/var/log/hermes not found, creating directory\n'
+		os.makedirs('/var/log/hermes')
+	#done check_log_dir()
 
-def sendthread(mssg):
-	quit = False
-	data = ""
-	print mssg
-	if (mssg == 1):
-		global Client
-		while quit == False:
-			data = raw_input()
-			print "[Me]>" + data
-			Client.send(data)
-			if (data == "quit"):
-				quit = True
-	if (mssg == 2):
-		while quit == False:
-			global cs
-			data = raw_input()
-			print "[Me]>" + data
-			cs.send(data)
-			if (data == "quit"):
-				quit = True
-
-#sets up the connection with the server and returns client information
-def connect_to_server(Address):
+def client_socket(Server_Address, Server_Port):
+	global logfile 
+	logfile.write('Createing Clinet Socket\n')
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ServerAddr = "67.241.38.178"
-	Port = 5000
-	s.connect((ServerAddr, Port))
-	check = "true"
-	Incomming = s.recv(1024)
-	print Incomming
-	s.send(Address)
-	#print "Address sent: " + Address
-	s.send(UserName)
-	#print "User Name Sent: " + UserName
-	#s.send(ConnectTo)  #removing to test
-	#print "Other User sent"
-	Incomming = s.recv(1024) # recv true/false
-	if(Incomming == check):
-		outgoing = "Client Entered Server Portion of Code"
-		print outgoing
-		s.send(outgoing)
-		Port = s.recv(1024)
-		Port = int(Port)
-		print Port
-		s.close()
-		Server_Code(Address, Port)
-	else:
-		outgoing = "Client Entered Client Portion of Code"
-  		print outgoing 
-		s.send(outgoing) #
-		ServerAddr = s.recv(1024) #
-		#print "Client Version of Client-Server address: " + ServerAddr
-		s.send(outgoing) #
-		Port = s.recv(1024)
-		Port = int(Port)
-		#print "Client Version of Port: ", Port
-		s.close()
-		print "Closed connection to Server"
-		Client_Code(ServerAddr, Port)
-	return
-		
+	logfile.write('Clinet Socket Created\n')
+	logfile.write('Connecting to Server\n')
+	s.connect((Server_Address, Server_Port))
+	logfile.write('Connection Created\n')
+	return s
+	#end create_socket()
 
-def Server_Code(Address, Port):
-	global Client
-	print "Creating Server Socket"
+def server_socket(Port):
+	global logfile
+	logfile.write('Creating Server Socket\n')
 	ServerS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	Port = int(Port)
-	print Port
-	Address = str(Address)
-	print Address
+	logfile.write('Socket built succesfully\n')
+	Port = 5000
+	Address = get_local_ip()
 	ServerS.bind((Address, Port))
-	print "Server Socket created, waiting for connection:"
+	logfile.write('Waiting for a Connection\n')
 	ServerS.listen(5)
-	Client, ClientAddr = ServerS.accept()
-	print "Got Connection from", ClientAddr
-	x = "Connected to: " + Address
-	Client.send(x)
-	
-	################################
-	#####Server Messaging Part:#####
-	################################
-	t1 = Thread(target=recvthread, args=(1,))
-	t2 = Thread(target=sendthread, args=(1,))
-	t1.start()
-	t2.start()
-	t1.join()
-	t2.join()
-	
-	ServerS.close()
-	print "end of server portion"
-	return
-	
-	
-def Client_Code(ServerAddr, Port):
-	global cs
-	print "Connecting to Other Client"
-	cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	cs.connect((ServerAddr, Port)) # Make sure to check Firewall
-	print "Socket Created"
-	Incomming = cs.recv(1024)
-	print Incomming
-	
-	################################
-	#####Client Messaging Part:#####
-	################################
-	t1 = Thread(target=recvthread, args=(2,))
-	t2 = Thread(target=sendthread, args=(2,))
-	t1.start()
-	t2.start()
-	t1.join()
-	t2.join()
-	
-	
-	print "All sockets Closed"
-	cs.close()
-	return
+	return ServerS
 
-#-----------------------------------------------------------#
-##############Start Calling Functions for use!###############
-#-----------------------------------------------------------#
-Client = ""
-cs = ""
+def server_exchange(ServerAddr):
+	global logfile
+	s = client_socket(ServerAddr, 5000)
+	#order is UserA, UserB, ClientIP
+	senddata = ["" for x in range(3)]
+	print "Enter Your User Name:"
+	senddata[0] = raw_input('>')
+	print "Enter User to Connect To:"
+	senddata[1] = raw_input('>')
+	senddata[2] = get_local_ip()
+	send_data_tmp = pickle.dumps(senddata)
+	s.send(str(send_data_tmp))
+	recvdata = ["" for x in range(5)]
+	recv_data_tmp = s.recv(2048)
+	print recv_data_tmp #<<<<<<<<<<<<<<REMOVE THIS LINE AFTER FIXED>>>>>>>>>>>>>>>
+	recvdata = pickle.loads(recv_data_tmp)
+	print recvdata
+	s.close()
+	#end server_exchange()
 
-Address = get_local_ip()
-UserName, ConnectTo = get_user_input()
-SendAddress = UserName
-connect_to_server(Address)
+def main():
+	global logfile
+	check_log_dir()
+	logfile = open('/var/log/hermes/connection.log', 'a+')
+	logfile.write("***File Opened***\n")
+	ServerAddr = "67.241.38.178" #"127.0.0.1"
+	server_exchange(ServerAddr)
+	#end main()
 
-#Gets client external address
-#Address = subprocess.check_output("wget http://people.sunyit.edu/~greenli/ip.php -qO -", shell=True)
+
+if __name__ == '__main__':
+	main()
