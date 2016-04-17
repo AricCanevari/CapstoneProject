3ceef11fbd3a5a0bbbe4d9ec7ce8077bd8f57aef
Aric Canevari <canevaa@sunyit.edu>
2016-04-17 18:35:08 -0400

    Update Server-5000.py

--- Server-5000.py
+++ Server-5000.py
@@ -196,7 +196,7 @@ def client_exchange(sessionlist, ServerS):
 		sessionlist[nextopenspot][1] = recvdata[1]
 		sessionlist[nextopenspot][2] = recvdata[2]
 		sessionlist[nextopenspot][3] = 5005
-		sessionlist[nextopenspot][4] = Random.new().read(AES.block_size)
+		sessionlist[nextopenspot][4] = Random.new().read(16)
 		print sys.getsizeof(sessionlist[nextopenspot][4])
 		sessionlist[nextopenspot][5] = Random.new().read(16)
 		print sys.getsizeof(sessionlist[nextopenspot][5])
